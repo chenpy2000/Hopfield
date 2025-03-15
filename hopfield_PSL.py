71ec222a78e16ce93ecc7d36fb42d103e2ab412d
CodingPotatoes <tianranbao@gmail.com>
2025-03-14 19:57:21 -0700

    added gary's recommendations to phase state learning and updated visualize_PSL to match

--- hopfield_PSL.py
+++ hopfield_PSL.py
@@ -3,16 +3,43 @@ import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader
 
+def init_weights(m):
+    if isinstance(m, nn.Linear):
+        torch.nn.init.xavier_uniform_(m.weight)
+        if m.bias is not None:
+            torch.nn.init.zeros_(m.bias)  # zero-initialize bias
+
 class HopfieldPSL(nn.Module):
     def __init__(self):
         super(HopfieldPSL, self).__init__()
-        self.skip = nn.Linear(1024, 1024)
+        
+        self.embed = nn.Embedding(1024, 2048)
+        self.skip = nn.Linear(2048, 1024)
         self.down = nn.Linear(1024, 100)
         self.up   = nn.Linear(100, 1024)
         self.activation = nn.Tanh()
 
+        self.apply(init_weights)
+
     def forward(self, x):
-        x_skip = self.skip(x)
-        x_hidden = self.activation(self.down(x))
-        x_hidden = self.activation(self.up(x_hidden))
-        return x_skip + x_hidden
+        states = []
+
+        for _ in range(5):
+            x_skip = self.skip(x)
+            embedding = self.embed(x)
+            x_hidden = self.activation(self.down(embedding))
+            x_hidden = self.activation(self.up(x_hidden))
+            x = x_skip + x_hidden  
+            states.append(x)
+
+        return states
+    
+    def retrieve_pattern(self, x):
+        for _ in range(5):
+            x_skip = self.skip(x)
+            embedding = self.embed(x)
+            x_hidden = self.activation(self.down(embedding))
+            x_hidden = self.activation(self.up(x_hidden))
+            x = x_skip + x_hidden  
+
+        return x 
